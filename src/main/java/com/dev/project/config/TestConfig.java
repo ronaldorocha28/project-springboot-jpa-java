a4a5bb067d1b02e31554fca39131c03e4f5ad0f8
Ronaldo Rocha <ronaldo.rocha28@hotmail.com>
2022-02-04 17:56:30 -0300

    Payment, one-to-one association

--- src/main/java/com/dev/project/config/TestConfig.java
+++ src/main/java/com/dev/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.dev.project.entities.Category;
 import com.dev.project.entities.Order;
 import com.dev.project.entities.OrderItem;
+import com.dev.project.entities.Payment;
 import com.dev.project.entities.Product;
 import com.dev.project.entities.User;
 import com.dev.project.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
--- src/main/java/com/dev/project/entities/Order.java
+++ src/main/java/com/dev/project/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.dev.project.entities.enums.OrderStatus;
@@ -32,6 +34,9 @@ public class Order implements Serializable {
 	private Instant moment;
 	
 	private Integer orderStatus;
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -87,6 +92,14 @@ public class Order implements Serializable {
 	
 	public Set<OrderItem> getItems(){
 		return items;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	@Override
